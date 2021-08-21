2bf0aff243bbbf3429a5150dce1abd5303c7b3ba
KeithDev2020 <eire19@hotmail.com>
2021-08-21 17:16:25 +0100

    Converted to Kotlin, Implemented Action bar, Began developing Main Activity UI with card views

--- app/src/main/java/com/example/multialarmclock/MainActivity.kt
+++ app/src/main/java/com/example/multialarmclock/MainActivity.kt
@@ -1,14 +1,66 @@
-package com.example.multialarmclock;
+package com.example.multialarmclock
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.ImageButton
+import androidx.appcompat.widget.AppCompatButton
+import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat
+import android.graphics.drawable.AnimatedVectorDrawable
+import androidx.annotation.RequiresApi
+import android.os.Build
+import android.os.Bundle
+import com.example.multialarmclock.R
+import android.graphics.drawable.Drawable
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
 
-import android.os.Bundle;
+class MainActivity : AppCompatActivity() {
+    var settings: ImageButton? = null
+    var avd: AnimatedVectorDrawableCompat? = null
+    var avd2: AnimatedVectorDrawable? = null
+    var toolbar: Toolbar? = null
+    var imageButtonTopLeft: AppCompatButton? = null
+    var imageButtonTopRight: AppCompatButton? = null
+    private var menu: Menu? = null
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        toolbar = findViewById(R.id.mytoolbar)
+        setSupportActionBar(toolbar)
 
-public class MainActivity extends AppCompatActivity {
+        imageButtonTopLeft = findViewById(R.id.topLeftImageButton)
+        imageButtonTopRight = findViewById(R.id.topRightImageButton)
+        //        settings = findViewById(R.id.settingsButton);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        Drawable drawable = menu.getItem(0).getIcon();
+//
+//        if(drawable instanceof AnimatedVectorDrawableCompat){
+//            avd = (AnimatedVectorDrawableCompat) drawable;
+//            avd.start();
+//        }else if(drawable instanceof AnimatedVectorDrawable){
+//            avd2 = (AnimatedVectorDrawable) drawable;
+//            avd2.start();
+//        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.menu_main, menu)
+        this.menu = menu
+        return true
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val drawable = item.icon
+        if (drawable is AnimatedVectorDrawable) {
+            avd = drawable as AnimatedVectorDrawableCompat
+            avd!!.start()
+        } else if (drawable is AnimatedVectorDrawable) {
+            avd2 = drawable
+            avd2!!.start()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
